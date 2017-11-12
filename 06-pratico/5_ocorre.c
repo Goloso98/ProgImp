eeef881c11ae0b2bcfcd36ea80c73deb169f8ac8
Goloso98 <gooloso98@gmail.com>
2017-11-12 20:31:42 +0000

    Update 5_ocorre.c
    better

--- 06-pratico/5_ocorre.c
+++ 06-pratico/5_ocorre.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#define MAX 100
+
 int ocorre(int vec[], int size, int val)
 {
   for (int i = 0; i < size; ++i)
@@ -14,5 +16,27 @@ int ocorre(int vec[], int size, int val)
 
 int main(void)
 {
+  int valor_lido[MAX], i = 0, numero;
+  
+  printf("Indique os valores(termina com 0 ou maximo de 100 valores):\n");
+  scanf("%d", &valor_lido[i]);
+  while (valor_lido[i] && i < MAX)
+  {
+    i++;
+    scanf("%d", &valor_lido[i]);
+  }
 
+  printf("Qual o numero a procurar: ");
+  scanf("%d", &numero);
+
+  if (ocorre(valor_lido, i, numero))
+  {
+    printf("Existe pelo menos um %d no vetor.\n", numero);
+  }
+  else
+  {
+    printf("Nao existe '%d' no vetor.\n", numero);
+  }
+
+  return 0;
 }
