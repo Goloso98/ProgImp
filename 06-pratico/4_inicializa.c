7b80eb4eeea96bca9437ba5913a6f5c73de78b1e
Goloso98 <gooloso98@gmail.com>
2017-11-12 20:18:31 +0000

    Update 4_inicializa.c

--- 06-pratico/4_inicializa.c
+++ 06-pratico/4_inicializa.c
@@ -1,16 +1,30 @@
 #include <stdio.h>
 
-int main(int argc, char const *argv[])
-{
-    /* code */
-    return 0;
-}
-
 void inicializa(int vec[], int size, int val)
 {
-    /* completar esta definição */
-    for (int i = 0; i < size; ++i)
-    {
-        vec[i] = val;
-    }
+  /* completar esta definição */
+  for (int i = 0; i < size; ++i)
+  {
+    vec[i] = val;
+  }
+}
+
+int main(void)
+{
+  int tamanho, valor, i = 0;
+  printf("Tamanho do vetor: ");
+  scanf("%d", &tamanho);
+  printf("Preencher com o valor: ");
+  scanf("%d", &valor);
+
+  int vetor[tamanho];
+  inicializa(vetor, tamanho, valor);
+
+  printf("Vetor: {");
+  for (i = 0; i < tamanho - 1; ++i)
+  {
+    printf("%d, ", vetor[i]);
+  }
+  printf("%d}\n", vetor[i]);
+  return 0;
 }
